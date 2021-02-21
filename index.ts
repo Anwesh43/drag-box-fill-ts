dc4bc15717d74bc23e25be858f67f158a7cee6fb
Anwesh43 <anweshthecool0@gmail.com>
2021-02-21 21:07:53 +0530

    adding constants

---
+++ index.ts
@@ -0,0 +1,7 @@
+const w : number = window.innerWidth 
+const h : number = window.innerHeight 
+const scGap : number = 0.02 
+const delay : number = 20
+const strokeFactor : number = 90 
+const backColor : string = "#bdbdbd"
+const color : string = "indigo"
