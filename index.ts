5c2a6993faf5214936233c790c55e4aa7b6413be
Anwesh43 <anweshthecool0@gmail.com>
2021-02-21 21:11:59 +0530

    adding State class

--- index.ts
+++ index.ts
@@ -5,3 +5,25 @@ const delay : number = 20
 const strokeFactor : number = 90 
 const backColor : string = "#bdbdbd"
 const color : string = "indigo"
+
+class State {
+
+    scale : number = 0 
+    dir : number = 0 
+
+    update(cb : Function) {
+        this.scale += scGap * this.dir 
+        if (this.scale > 1 ) {
+            this.scale = 1 
+            this.dir = 0 
+            cb()
+        }
+    }
+
+    startUpdating(cb : Function) {
+        if (this.dir == 0) {
+            this.dir = 1 
+            cb()
+        }
+    }
+}
